a9aad6f12e65f7e97d308f6cbc6a3225bd85177f
JidroZ <zoujizhuo524@163.com>
2022-11-04 10:53:17 +0800

    change sudo

--- i3status/install.sh
+++ i3status/install.sh
@@ -1,19 +1,19 @@
 #!/bin/env bash
 
-sudo cp ~/.config/i3status/config ~/.config/i3status/config.bak
-sudo rm -rf ~/.config/i3status/config
-sudo cp i3status.conf ~/.config/i3status
-sudo mv ~/.config/i3status/i3status.conf ~/.config/i3status/config
+cp -rfpv ~/.config/i3status/config ~/.config/i3status/config.bak
+rm -rfv ~/.config/i3status/config
+cp -rfpv i3status.conf ~/.config/i3status
+mv -v ~/.config/i3status/i3status.conf ~/.config/i3status/config
 
-sudo cp /etc/xdg/i3status/config /etc/xdg/i3status/config.bak
-sudo rm -rf /etc/xdg/i3status/config
-sudo cp i3status.conf /etc/xdg/i3status
-sudo mv /etc/xdg/i3status/i3status.conf /etc/xdg/i3status/config
+sudo cp -rfpv /etc/xdg/i3status/config /etc/xdg/i3status/config.bak
+sudo rm -rfv /etc/xdg/i3status/config
+sudo cp -rfpv i3status.conf /etc/xdg/i3status
+sudo mv -v /etc/xdg/i3status/i3status.conf /etc/xdg/i3status/config
 
-sudo cp ~/.i3status.conf ~/.i3status.conf.bak
-sudo rm -rf ~/.i3status.conf
-sudo cp i3status.conf ~/
+cp -rfpv ~/.i3status.conf ~/.i3status.conf.bak
+rm -rfv ~/.i3status.conf
+cp -rfpv i3status.conf ~/
 
-sudo cp /etc/i3status.conf /etc/i3status.conf.bak
+sudo cp -rfpv /etc/i3status.conf /etc/i3status.conf.bak
 sudo rm -rf /etc/i3status.conf
-sudo cp i3status.conf /etc
+sudo cp -rfpv i3status.conf /etc
