cfcf75dc4f4e8afb449c9bb62f1320a61e0a6e0b
Jidro.Z <z3069566588@163.com>
2023-05-04 15:09:59 +0800

    Add comments to the wallpaper script.

--- i3blocks/scripts/wallpapers/wallpapers.sh
+++ i3blocks/scripts/wallpapers/wallpapers.sh
@@ -1,8 +1,28 @@
 #!/bin/env bash
 
+# feh is a lightweight image viewer aimed mainly 
+# at users of command line interfaces.
+# Unlike most graphical image viewers, 
+# feh does not have any graphical control elements 
+# (apart from an optional file name display) 
+# which enables it to also be used to display background images 
+# on systems running the X window system.
+
+# This script will use feh to set the background image. 
+# Please download and install the feh application software 
+# before using this script to configure the background image.
 while true; do
-	# ~/Wallpapers是存放壁纸的文件夹
-	feh --recursive --randomize --bg-fill --no-fehbg ~/Wallpapers
-	# 每600秒自动切换壁纸
+	# Parameter '--no-fehbg' indicates the folder 
+	# where the background image resides. 
+	# Please replace it with the folder name you need. 
+	# By default, the backgrounds are searched and set 
+	# from /usr/share/backgrounds folder.
+	#feh --recursive --randomize --bg-scale --no-fehbg $HOME/.local/share/wallpapers/
+	#feh --recursive --randomize --bg-scale --no-fehbg /usr/share/backgrounds/
+	feh --recursive --randomize --bg-scale --no-fehbg /usr/share/backgrounds/
+	#feh --recursive --randomize --bg-scale --no-fehbg /usr/share/wallpapers/
+	# The parameters in the following line are 
+	# set to automatically change the desktop wallpaper 
+	# every 600 seconds.
 	sleep 600
 done
